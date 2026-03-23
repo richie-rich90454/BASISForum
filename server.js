fe2dd2d7e1384dc7bd89e9a018942f9fa0e85642
Gavinotfound <101452310+Gavinotfound@users.noreply.github.com>
2026-03-23 16:36:38 +0800

    Change default server port to 3k

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const sanitizeHtml = require('sanitize-html');
 const rateLimit = require('express-rate-limit');
 
 const app = express();
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
